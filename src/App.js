57945d7ea1244bcdc8d601e375a774caed2eaa15
go367it <satyamkumar3001@gmail.com>
2022-05-03 09:00:05 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDUcj3Vzvrtx4QzkhBfRVIBbZA-ivOyHlI",
+  authDomain: "superchat-da549.firebaseapp.com",
+  projectId: "superchat-da549",
+  storageBucket: "superchat-da549.appspot.com",
+  messagingSenderId: "960349098249",
+  appId: "1:960349098249:web:7ad50e3ac7657b35f0c3d8",
+  measurementId: "G-WSB1H3TFVL"
+};
+
+firebase.initializeApp({
+  firebaseConfig
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
+
+  const [user] = useAuthState(auth)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
